6d7ca1c5a9e4e117b1755a6505cd3549a26c1c94
krendel506 <krendel506@gmail.com>
2024-09-29 11:41:49 +0300

    modified code printf part

--- hw1.c
+++ hw1.c
@@ -7,56 +7,57 @@ int main(){
     int h=0; //step
     int c=0;
     int n=0;
-    float x ;
+    float y ;
 
-    int trash = 0;
+    int temporaryStorage = 0;
 
     printf("Enter x value: ");
     scanf("%f",&a);
 
     while(true){
         printf("Enter H: ");
-        scanf("%d",&trash);
-     if(trash > 0){
-        h= trash;
+        scanf("%d",&temporaryStorage);
+     if(temporaryStorage > 0){
+        h= temporaryStorage;
         break;          
      }
     }
 
     while(true){
         printf("Enter C: ");
-        scanf("%d",&trash);
-     if(trash >= 0){
-        c= trash;
+        scanf("%d",&temporaryStorage);
+     if(temporaryStorage >= 0){
+        c= temporaryStorage;
         break;
      }
     }
      
     while(true){
         printf("Enter N: ");
-        scanf("%d",&trash);
-     if(trash > 0){
-        n = trash;
+        scanf("%d",&temporaryStorage);
+     if(temporaryStorage > 0){
+        n = temporaryStorage;
         break;
      }
     }
 
- // Variables to store current power of C and sSide for the loop
+    printf("X           Y\n");
+     // Variables to store current power of C and sSide for the loop
      float currentPower = 1.0f;  // C^0 = 1 for the first iteration
 
     for (int i = 0; i < n; i++) {
-        float sSide = 0;
+        float aCurrentValue = 0;
 
         if (i == 0) {
-            sSide = 0;  // First iteration, sSide is 0
+            aCurrentValue = 0;  // First iteration, sSide is 0
         } else if (i == 1) {
-            sSide = h;  // Second iteration, sSide is h
+            aCurrentValue = h;  // Second iteration, sSide is h
         } else {
             currentPower *= c;  // Iteratively calculate c^(i-1)
-            sSide = currentPower * h;
+            aCurrentValue = currentPower * h;
         }
 
-        a += sSide;  // Update value of 'a'
+        a += aCurrentValue;  // Update value of 'a'
         float a_square = powf(a,2);
         float bottomSide = a_square + a - 10;
         float squareRootExpression = a_square + a - 20;
@@ -65,12 +66,12 @@ int main(){
     because x<-5 and x>4
     while for bottomSide x<3.7 and x>2.7*/
       if(squareRootExpression < 0){
-         printf("if x=%f then y is imaginary\n",a);
+         printf("%-10.2f  missing \n",a);
       }
       else{
         float squareRoot = sqrt(squareRootExpression);
-        x = squareRoot / bottomSide;    
-        printf("%f\n",x);
+        y = squareRoot / bottomSide;    
+        printf("%-10.2f  %-10.6f \n",a,y);
       }  
     } 
     

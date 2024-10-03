97c1b81b6cdd22c38edc0ba165bdab28116c6122
krendel506 <krendel506@gmail.com>
2024-10-03 10:10:59 +0300

    Added functions and prototypes

--- hw1.c
+++ hw1.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 
+void inputingVariables(float *a, float *h, float *c, int *n);
+void functionCalculation(float a, float c, float h, int n);
+
 int main(){
     float a=0;
     float h=0;
     float c=0;
     int n=0;
-    float x,y;
+    
     
     //Inputing variables
-    printf("Enter x value: ");
-    scanf("%f",&a);
+   inputingVariables(&a,&h,&c,&n);
+   //printing blueprint for table
+    printf("X           Y\n");
+   //calculating the value
+    functionCalculation(a,c,h,n);
+}
 
+void inputingVariables(float *a, float *h, float *c,int *n){
+     printf("Enter x value: ");
+     scanf("%f",a);
     do{
         printf("Enter a value for H (H>0): ");
-        scanf("%f",&h);
+        scanf("%f",h);
     }while(h<=0);
 
     do{
-        printf("Enter value for C (C>1): ");
-        scanf("%f",&c);
+        printf("Enter value for C (C=>1): ");
+        scanf("%f",c);
     }while(c<=1);
      
     do{
         printf("Enter N: ");
-        scanf("%d",&n);
+        scanf("%d",n);
     }while(n<0);
+}
 
-
-    printf("X           Y\n");
-    //calculating the value
+void functionCalculation(float a, float c, float h, int n)
+{
     for (int i = 0; i < n; i++) {
+        float x,y;
         x= a + pow(c,i)*h;
         y= sqrt(pow(x,2) + x - 20) / (pow(x,2) + x - 10);
         
@@ -41,5 +52,4 @@ int main(){
             printf("%-10.2f  %-10.6f \n",x,y);
             }
     } 
-    
 }
